57bff7232bcd8e3a867ac18d1523d433fdc715f1
Michael DeCicco <mdecicco8@gmail.com>
2024-08-30 02:17:58 -0400

    moved logging utils to utils dep

--- src/interfaces/ILogHandler.cpp
+++
@@ -1,7 +0,0 @@
-#include <codegen/interfaces/ILogHandler.h>
-
-namespace codegen {
-    void ILogHandler::onInfo(const char* msg) { }
-    void ILogHandler::onWarn(const char* msg) { }
-    void ILogHandler::onError(const char* msg) { }
-};
