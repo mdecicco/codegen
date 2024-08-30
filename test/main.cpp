e3eea5c8453520f63193af5205cf0af7a368b56b
Michael DeCicco <mdecicco8@gmail.com>
2024-08-30 02:19:44 -0400

    update test

--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,43 @@
 #include <codegen/FunctionBuilder.h>
+#include <utils/interfaces/ILogHandler.h>
 #include <bind/bind.h>
 using namespace codegen;
 using namespace bind;
 using namespace utils;
 
-struct test_struct {
+struct test_struct_1 {
     i32 a, b, c;
+};
+
+struct test_struct_2 {
+    i32 a, b, c;
+    test_struct_1 d;
+};
+
+class ErrHandler : public ILogHandler {
+    virtual void onDebug(const char* msg) {
+        printf("Debug: %s\n", msg);
+        fflush(stdout);
+    }
+    virtual void onInfo(const char* msg) {
+        printf("Info: %s\n", msg);
+        fflush(stdout);
+    }
+
+    virtual void onWarn(const char* msg) {
+        printf("Warn: %s\n", msg);
+        fflush(stdout);
+    }
+
+    virtual void onError(const char* msg) {
+        printf("Error: %s\n", msg);
+        fflush(stdout);
+    }
 };
 
 int main(int argc, const char** argv) {
     Registry::Create();
-    
+
     {
         build<void>("void");
         build<bool>("bool");
@@ -24,13 +51,23 @@ int main(int argc, const char** argv) {
         build<u64>("u64");
         build<f32>("f32");
         build<f64>("f64");
-        auto ts = build<test_struct>("ivec3");
-        ts.prop("a", &test_struct::a);
-        ts.prop("b", &test_struct::b);
-        ts.prop("c", &test_struct::c);
+
+        auto ts1 = build<test_struct_1>("test1");
+        ts1.prop("a", &test_struct_1::a);
+        ts1.prop("b", &test_struct_1::b);
+        ts1.prop("c", &test_struct_1::c);
+
+        auto ts2 = build<test_struct_2>("test2");
+        ts2.prop("a", &test_struct_2::a);
+        ts2.prop("b", &test_struct_2::b);
+        ts2.prop("c", &test_struct_2::c);
+        ts2.prop("d", &test_struct_2::d);
+
+        ErrHandler h;
 
         Function fn = Function("test", Registry::Signature<i32, i32, i32>(), Registry::GlobalNamespace());
         FunctionBuilder fb = FunctionBuilder(&fn);
+        fb.setLogHandler(&h);
 
         fb.enableValidation();
         Value arg1 = fb.getArg(0);
@@ -40,31 +77,47 @@ int main(int argc, const char** argv) {
         arg1.setName("arg1");
         arg2.setName("arg2");
         something.setName("something");
+
+        Value v2 = fb.val(ts2.getType());
+        fb.generateConstruction(v2, {});
+        v2.setName("vec2");
+        fb.store(fb.val(0), v2, offsetof(test_struct_2, a));
+        fb.store(fb.val(1), v2, offsetof(test_struct_2, b));
+        fb.store(fb.val(2), v2, offsetof(test_struct_2, c));
         
         fb.generateIf(something > fb.val(10), [&](){
+            Scope s (&fb);
             something += arg2;
 
             Value i = fb.val(Registry::GetType<u32>());
             i.setName("i");
             i = fb.val(u32(0));
-            fb.generateFor([&]() -> Value{
+            fb.generateFor([&](){
                 return i < fb.val(10);
             }, [&](){
                 i++;
             }, [&](){
                 something += i;
+                fb.generateIf(something > i, [&]() {
+                    fb.generateReturn(i);
+                });
             });
 
-            Value v = fb.val(ts.getType());
-            v.setName("vec");
-            fb.store(fb.val(0), v, offsetof(test_struct, a));
-            fb.store(fb.val(1), v, offsetof(test_struct, b));
-            fb.store(fb.val(2), v, offsetof(test_struct, c));
+            Value v1 = fb.val(ts1.getType());
+            v1.setName("vec1");
+            fb.generateConstruction(v1, { v2 });
+            fb.store(fb.val(0), v1, offsetof(test_struct_1, a));
+            fb.store(fb.val(1), v1, offsetof(test_struct_1, b));
+            fb.store(fb.val(2), v1, offsetof(test_struct_1, c));
+
+            fb.generateIf(something > arg1, [&]() {
+                fb.generateReturn(i);
+            });
 
             something -= fb.generateCall(&fb, { arg1, arg2 });
         });
 
-        fb.ret(something);
+        fb.generateReturn(something);
 
         auto code = fb.getCode();
         for (u32 i = 0;i < code.size();i++) {
