4e0d3ab577408cc0881db5c169f6c16f74413c16
Michael DeCicco <mdecicco8@gmail.com>
2024-09-21 18:20:32 -0400

    removed space

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch_session.hpp>
 #include "Common.h"
 
-int main(int argc, const char* argv[] ) {
+int main(int argc, const char* argv[]) {
     Registry::Create();
     int result = Catch::Session().run(argc, argv);
     Registry::Destroy();
